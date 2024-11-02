70274441326a1104d6c108e05d03dedf2d41be14
Nasr <nasr.asri10@gmail.com>
2024-11-02 15:48:04 +0100

    fix import directory of WelcomMessage

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,4 @@
-import WelcomeMessage from './WelcomeMessage
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -9,6 +9,7 @@ function App() {
 
   return (
     <>
+    <WelcomeMessage />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
